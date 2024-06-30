3de77e6faf54c35a1aa03c256276643c54cec8e0
Jonathan Vincent <jonathan.vincent@siemens-healthineers.com>
2024-06-30 21:06:14 +0200

    Add analysis-report.pdf and modify transformation.py (add data type dictionary)

--- project/transformation.py
+++ project/transformation.py
@@ -35,6 +35,25 @@ def transform_eer_data(eer_df,df_type):
 
         # Rename the columns in the datagrame using the dictionary and return the transformed dataframe
         renamed_final_df = final_df.rename(columns=column_rename_dict)
+
+        # Set the correct data types for each column
+        dtype_dict = {
+            'Country Name': 'str',
+            'Year': 'int64',
+            'Electricity Demand (TWh)': 'float64',
+            'Electricity Demand per Capita (MWh)': 'float64',
+            'Fuel Code': 'str',
+            'Fuel Description': 'str',
+            'Electricity Generation (TWh)': 'float64',
+            'Share of Generation (%)': 'float64',
+            'Emissions Intensity (gCO2/kWh)': 'float64',
+            'Emissions (MTcO2e)': 'float64',
+        }
+        
+        for column, dtype in dtype_dict.items():
+            if column in renamed_final_df.columns:
+                renamed_final_df[column] = renamed_final_df[column].astype(dtype)
+                
         return renamed_final_df
     
     except Exception as e:
@@ -56,11 +75,17 @@ def transform_gdp_data(gdp_df):
         # Convert the dataframe from wide format to long format, create new column 'Year'
         final_gdp_df = pd.melt(filtered_columns_df, id_vars=id_vars, var_name='Year', value_name='GDP (US$)')
 
-        # Convert 'Year' column to numeric 
-        final_gdp_df['Year'] = pd.to_numeric(final_gdp_df['Year'])
-
-        # Convert the 'GDP (US$)' column to numeric and return transformed dataframe
+        # Convert 'Year' and 'GDP' to numeric, errors='coerce' will turn non-convertible values into NaN
+        final_gdp_df['Year'] = pd.to_numeric(final_gdp_df['Year'], errors = 'coerce').astype('Int64')
         final_gdp_df['GDP (US$)'] = pd.to_numeric(final_gdp_df['GDP (US$)'], errors='coerce')
+
+        # Drop rows where 'Year' and 'GDP' is NaN
+        final_gdp_df.dropna(subset=['Year', 'GDP (US$)'], inplace=True) 
+
+        # Now, divide by 1e9 to convert to billions for better understanding
+        final_gdp_df['GDP (US$)'] = final_gdp_df['GDP (US$)'] / 1e9
+        final_gdp_df.rename(columns={'GDP (US$)': 'GDP (Billion US$)'}, inplace=True)
+
         return final_gdp_df
     
     except Exception as e:
